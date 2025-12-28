3f3de518c601a71c1351fd78be6cf40e510a113c
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-28 14:33:20 -0800

    fix: default QR code schema fullscreen parameter to false

--- lynx.config.ts
+++ lynx.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     pluginQRCode({
       schema(url) {
         // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
-        return `${url}?fullscreen=true`
+        return `${url}?fullscreen=false`
       },
     }),
     pluginReactLynx(),
