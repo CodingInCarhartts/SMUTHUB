09bfb92932ce1f86aa399a4c1994e683136a4de4
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-29 07:16:19 -0800

    🔖 native: release v1.0.29 (code 24)

--- src/services/update.ts
+++ src/services/update.ts
@@ -80,7 +80,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const APP_VERSION = '1.0.28';
+export const APP_VERSION = '1.0.29';
 
 export const UpdateService = {
   /**
