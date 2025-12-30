e5e907143280dd4afe8d124f33db16af71f6ba9b
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-29 21:53:25 -0800

    🚀 ota: Forced Bundle Commit (v1.0.48)

--- src/services/update.ts
+++ src/services/update.ts
@@ -53,7 +53,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const APP_VERSION = '1.0.47';
+export const APP_VERSION = '1.0.48';
 
 export const UpdateService = {
   /**
