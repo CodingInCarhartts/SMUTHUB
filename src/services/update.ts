61cfe9df5a897dfe4c7696b18da390408f840023
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-29 21:37:06 -0800

    🚀 ota: Final OTA Integrity Check (v1.0.43)

--- src/services/update.ts
+++ src/services/update.ts
@@ -53,7 +53,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const APP_VERSION = '1.0.42';
+export const APP_VERSION = '1.0.43';
 
 export const UpdateService = {
   /**
