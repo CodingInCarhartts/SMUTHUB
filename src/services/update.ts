df23707f8eb8d1503eaa5a199dc9b21c2b104e21
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-29 21:28:57 -0800

    🚀 ota: Reliability fix for sparkles (v1.0.39)

--- src/services/update.ts
+++ src/services/update.ts
@@ -81,7 +81,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const APP_VERSION = '1.0.37';
+export const APP_VERSION = '1.0.39';
 
 export const UpdateService = {
   /**
