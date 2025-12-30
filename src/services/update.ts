0a20ba39ca65192317342be5f8dc1d2f72dcbc93
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-30 09:16:37 -0800

    🚀 ota: Style Updates (v1.0.72)

--- src/services/update.ts
+++ src/services/update.ts
@@ -53,7 +53,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const BUNDLE_VERSION = '1.0.71';
+export const BUNDLE_VERSION = '1.0.72';
 
 export const UpdateService = {
   /**
