1a412603912ee01b55ca05a33e687a2dea4b35bd
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-30 17:43:09 -0800

    🚀 ota: Enable Commit Hash OTA

--- src/services/update.ts
+++ src/services/update.ts
@@ -56,7 +56,7 @@ export interface NativeAppUpdate {
 }
 
 export const BUNDLE_VERSION = '1.0.83';
-export const BUNDLE_COMMIT_HASH = '2cf15a0'; // Will be injected by publish-ota.js
+export const BUNDLE_COMMIT_HASH = '8e532cf'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
