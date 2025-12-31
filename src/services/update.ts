9e34ed849d92d8703c9d7dd500628e8629751dc9
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-30 16:55:10 -0800

    🚀 ota: UI Attempt v2: Version Update (v1.0.79)

--- src/services/update.ts
+++ src/services/update.ts
@@ -53,7 +53,7 @@ export interface NativeAppUpdate {
   forceImmediate: boolean;
 }
 
-export const BUNDLE_VERSION = '1.0.78';
+export const BUNDLE_VERSION = '1.0.79';
 
 export const UpdateService = {
   /**
