da09676839e3b9d435c272fd04f6b1e0d8e6d82e
CodingInCarhartts <TS.Shinigami@proton.me>
2025-12-30 19:56:03 -0800

    🔖 native: bump version to 1.1.1

--- SMUTHUB/app/build.gradle.kts
+++ SMUTHUB/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.smuthub"
         minSdk = 24
         targetSdk = 34
-        versionCode = 44
-        versionName = "1.1.0"
+        versionCode = 45
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
