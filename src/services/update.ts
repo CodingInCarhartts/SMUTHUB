092f2b4e0b1a0d0371a46c140d0f975426d8a5a0
CodingInCarhartts <TS.Shinigami@proton.me>
2026-01-01 22:49:04 -0800

    🚀 ota: Style fixes

--- src/services/update.ts
+++ src/services/update.ts
@@ -55,8 +55,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.116';
-export const BUNDLE_COMMIT_HASH = 'f43a2ef'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.117';
+export const BUNDLE_COMMIT_HASH = 'f4f21d2'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
