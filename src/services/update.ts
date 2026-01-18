19b11182729450708b895273bb18b2e2f0ffb74b
CodingInCarhartts <TS.Shinigami@proton.me>
2026-01-17 22:00:36 -0800

    🚀 ota: TestUpdate for Panels

--- src/services/update.ts
+++ src/services/update.ts
@@ -55,8 +55,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.132';
-export const BUNDLE_COMMIT_HASH = 'e42fc60'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.133';
+export const BUNDLE_COMMIT_HASH = 'b44aa69'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
