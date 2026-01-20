0218702d7f0d0196f8cec37cf68a13e1d0b06a27
CodingInCarhartts <TS.Shinigami@proton.me>
2026-01-19 19:18:38 -0800

    🚀 ota: Fixed Batoto error spam (v2)

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.148';
-export const BUNDLE_COMMIT_HASH = 'eff2735'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.149';
+export const BUNDLE_COMMIT_HASH = '4159254'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
