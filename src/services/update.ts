3828dc2cbadffb756ee30c2439703fd822d12093
CodingInCarhartts <TS.Shinigami@proton.me>
2026-01-23 22:14:09 -0800

    🚀 ota: Final Stability: Fixed Config

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.183';
-export const BUNDLE_COMMIT_HASH = '445274f'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.185';
+export const BUNDLE_COMMIT_HASH = '070d94a'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
