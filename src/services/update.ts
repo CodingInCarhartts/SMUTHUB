d1214b3dc460e0f79b141ee42395d956c7b6252c
CodingInCarhartts <TS.Shinigami@proton.me>
2026-02-13 03:54:36 -0800

    🚀 ota: Add search filters support to Comix service

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.254';
-export const BUNDLE_COMMIT_HASH = '56a74b6'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.255';
+export const BUNDLE_COMMIT_HASH = '91049cb'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
