75a0d09177768a6596ba6b2c7dfb06b03785c121
CodingInCarhartts <TS.Shinigami@proton.me>
2026-02-12 21:54:55 -0800

    🚀 ota: Comix.to Testing

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.229';
-export const BUNDLE_COMMIT_HASH = '442254e'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.230';
+export const BUNDLE_COMMIT_HASH = '8284a34'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
