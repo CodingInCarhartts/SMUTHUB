96752d0324e0573f83d2b152607752f637b63d0d
CodingInCarhartts <TS.Shinigami@proton.me>
2026-02-13 03:45:41 -0800

    🚀 ota: Comix.to Panels

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.253';
-export const BUNDLE_COMMIT_HASH = '33362e3'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.254';
+export const BUNDLE_COMMIT_HASH = '56a74b6'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
