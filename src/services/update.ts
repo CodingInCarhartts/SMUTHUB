8491336a55be986a78d77bb41e6335f1444e7700
CodingInCarhartts <TS.Shinigami@proton.me>
2026-02-13 02:24:14 -0800

    🚀 ota: Comix.to Fix Verify

--- src/services/update.ts
+++ src/services/update.ts
@@ -59,8 +59,8 @@ export interface NativeAppUpdate {
   commitHash?: string;
 }
 
-export const BUNDLE_VERSION = '1.0.238';
-export const BUNDLE_COMMIT_HASH = 'f85298f'; // Will be injected by publish-ota.js
+export const BUNDLE_VERSION = '1.0.239';
+export const BUNDLE_COMMIT_HASH = 'd5f04d7'; // Will be injected by publish-ota.js
 
 export const UpdateService = {
   /**
